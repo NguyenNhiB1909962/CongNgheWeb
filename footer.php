92f96b0bee28ab9448792c676cde9b8a8b6231c7
nhi2206 <104351482+nhi2206@users.noreply.github.com>
2022-04-25 17:34:16 +0700

    partials

---
+++ footer.php
@@ -0,0 +1,7 @@
+<footer class="footer">
+    <div class="container ">
+        <div class="text-center">
+            <b class="text-muted"><h3></br> Copyright &copy; Websize Được Thiết Kế Bởi Double Nhi</h3></b>
+        </div>
+    </div>
+</footer>
