7dce03da3643c8626af651f376a946a58c3d5106
nhi2206 <104351482+nhi2206@users.noreply.github.com>
2022-04-25 17:36:57 +0700

    Delete footer.php

--- footer.php
+++
@@ -1,7 +0,0 @@
-<footer class="footer">
-    <div class="container ">
-        <div class="text-center">
-            <b class="text-muted"><h3></br> Copyright &copy; Websize Được Thiết Kế Bởi Double Nhi</h3></b>
-        </div>
-    </div>
-</footer>
